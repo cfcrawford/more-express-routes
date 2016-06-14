65b2a74046f57cefcec61ed86a847cab33e4a340
cfcrawford <camilafcrawford@gmail.com>
2016-06-14 09:17:47 -0700

    Edits file

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 var express = require('express'),
     app = express();
 
-
 var colors = [
 "red",
 "green",
@@ -16,6 +15,46 @@ app.get("/color/:index", function (req, res) {
     var index = req.params.index;
     var colorchoice = colors[index];
     res.send("You picked: " + colorchoice);
+});
+
+var burgers = [
+    "Cheeseburger",
+    "Bleu Cheese burger",
+    "Bacon Double cheese burger"
+];
+
+var tacos = [
+    "Tacos verdes",
+    "Tacos al pastor",
+    "Tacos grandes"
+];
+
+
+app.get("/taco/:index", function (req, res) {
+    var index = req.params.index;
+    var selection = tacos[index] || "Sorry, that's not a valid taco";
+    res.send("You picked: " + selection);
+});
+
+app.get("/burger/:index", function (req, res) {
+    var index = req.params.index;
+    var burgerchoice = burgers[index] || "Sorry, that's not a valid burger";
+    res.send("You picked: " + selection);
+});
+
+var correct_number = 10;
+
+app.get("/pick-a-number/:num", function(req, res) {
+  var num = req.params.num
+  if (num == correct_number){
+    res.send("That is the correct number!")
+  }
+    else (num > correct_number) {
+      res.send("This number is too high.")
+    }
+    else {
+      res.send("This number is too low.")
+    }
 });
 
 // start the server
